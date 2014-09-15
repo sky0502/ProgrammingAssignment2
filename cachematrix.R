56c7e0047b48f56badad58be9812bb1c6c4c86c7
Helen <helenyan624@gmail.com>
2014-09-15 18:26:57 -0400

    R programming assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+##cache the inverse of a matrix
 
-## Write a short comment describing this function
+##This function creats a special matrix that can cache its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x=matrix()){
+	inv<-NULL	
+#set the value of the matrix	
+	set<-function(y){
+		x<<-y
+		inv<<-NULL
+	}
+#get the value of matrix
+	get<-function() x
+#set the value of the inverse matrix
+	setinverse<-function(solve) inv<<-solve
+#get the value of the inverse matrix
+	getinverse<-function() inv
+	list(set=set, get=get, setinverse=setinverse, getinverse=getinverse)	
 }
 
+##This function computes the inverse of the special matrix returned by makeCacheMatrix. 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(x, ...){
+	inv<-x$getinverse()
+#If the inverse matrix is already calculated
+	if(!is.null(inv)){
+		message("getting cached data")
+		return(inv)
+	}
+	
+#calculate the inverse matrix and sets the value of the mean in the cache
+	matrix<-x$get()
+	inv<-solve(matrix)
+	x$setinverse(inv)
+	inv
 }
+
+#> m<-makeVector(1:10)
+#> m$get()
+# [1]  1  2  3  4  5  6  7  8  9 10
+#> cachemean(m)
+#[1] 5.5
+#> cachemean(m)
+#getting cached data
+#[1] 5.5
+
+#x<-matrix(1:4,nrow=2)
+#n<-makeCacheMatrix(x)
+#n$get()
+#cacheSolve(n)
+#cacheSolve(n)
